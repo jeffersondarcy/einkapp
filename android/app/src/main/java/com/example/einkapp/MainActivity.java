2ec530b267cb0e8c9e715a1641135eecc5e8d179
jeff <4200365+jeffersondarcy@users.noreply.github.com>
2020-05-14 20:37:10 +0200

    some progress

--- android/app/src/main/java/com/example/einkapp/MainActivity.java
+++ android/app/src/main/java/com/example/einkapp/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.einkapp;
 
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +20,7 @@ import android.hardware.display.VirtualDisplay;
 import android.media.projection.MediaProjection;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.view.Display;
 import android.view.Surface;
 import android.util.Log;
@@ -23,12 +30,17 @@ import android.view.WindowManager;
 import android.util.DisplayMetrics;
 import android.graphics.Point;
 
-public class MainActivity extends FlutterActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends FlutterActivity implements OnImageAvailableListener {
     private MediaProjectionManager mProjectionManager;
     private MediaProjection mMediaProjection;
     private VirtualDisplay mVirtualDisplay;
-    private Surface mSurface;
     int width, height, density;
+    ImageReader mImageReader;
     private static final String CHANNEL = "e-ink.fitdev.io/screen-capture";
     private static final String TAG = "MediaProjectionDemo";
     private static final int PERMISSION_CODE = 1;
@@ -42,13 +54,6 @@ public class MainActivity extends FlutterActivity {
         width = size.x;
         height = size.y;
         density = metrics.densityDpi;
-    }
-
-    private VirtualDisplay createVirtualDisplay() {
-        return mMediaProjection.createVirtualDisplay("ScreenSharingDemo",
-                width, height, density,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                mSurface, null /*Callbacks*/, null /*Handler*/);
     }
 
     @Override
@@ -74,9 +79,11 @@ public class MainActivity extends FlutterActivity {
             return;
         }
         mMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
-        mVirtualDisplay = createVirtualDisplay();
-
-
+        mImageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 10);
+        mImageReader.setOnImageAvailableListener(this, null);
+        final int flags = DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR;
+        mMediaProjection.createVirtualDisplay("screen-mirror", width, height, density, flags, mImageReader.getSurface(), null, null);
+        Log.w("mylog", "dsdfsfs serfsf");
     }
 
     @Override
@@ -110,4 +117,70 @@ public class MainActivity extends FlutterActivity {
         }
       );
   }
+
+    @Override
+    public void onImageAvailable(ImageReader reader) {
+        Image image = reader.acquireLatestImage();
+        Log.w("mylog", reader.toString());
+
+        FileOutputStream fos = null;
+            Bitmap bitmap = null;
+            Image img = null;
+            try {
+                img = reader.acquireLatestImage();
+                if (img != null) {
+                    Image.Plane[] planes = img.getPlanes();
+                    if (planes[0].getBuffer() == null) {
+                        return;
+                    }
+                    int width = img.getWidth();
+                    int height = img.getHeight();
+                    int pixelStride = planes[0].getPixelStride();
+                    int rowStride = planes[0].getRowStride();
+                    int rowPadding = rowStride - pixelStride * width;
+                    byte[] newData = new byte[width * height * 4];
+
+                    int offset = 0;
+                    bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                    ByteBuffer buffer = planes[0].getBuffer();
+                    for (int i = 0; i < height; ++i) {
+                        for (int j = 0; j < width; ++j) {
+                            int pixel = 0;
+                            pixel |= (buffer.get(offset) & 0xff) << 16;     // R
+                            pixel |= (buffer.get(offset + 1) & 0xff) << 8;  // G
+                            pixel |= (buffer.get(offset + 2) & 0xff);       // B
+                            pixel |= (buffer.get(offset + 3) & 0xff) << 24; // A
+                            bitmap.setPixel(j, i, pixel);
+                            offset += pixelStride;
+                        }
+                        offset += rowPadding;
+                    }
+                    String name = "/myscreen.jpg";
+                    File file = new File(Environment.DIRECTORY_PICTURES, name);
+                    fos = new FileOutputStream(file.getAbsolutePath());
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    img.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (null != fos) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (null != bitmap) {
+                    bitmap.recycle();
+                }
+                if (null != img) {
+                    img.close();
+                }
+
+            }
+        image.close();
+
+    }
 }
